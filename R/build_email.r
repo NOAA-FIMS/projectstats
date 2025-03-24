2fb5d4fbf97c0d370f6ed97ac8ee11eefe927160
kellijohnson-NOAA <kelli.johnson@noaa.gov>
2025-03-23 20:24:34 -0700

    doc(build_email): Fixes documentation code{} to md

--- R/build_email.r
+++ R/build_email.r
@@ -1,19 +1,28 @@
 #' Builds and sends the email
 #'
 #' This function compiles the tables with some email text and sends it
-#' using \code{blastula::smtp_send.}
+#' using `blastula::smtp_send()`.
 #'
 #' @param from_ sender email address
 #' @param to_ recipient email address
-#' @param creds A credential file of the type supported by code{blastula}
-#' @seealso [blastula::creds]
+#' @param creds A credential file of the type supported by {blastula}
 #' @param body_ The text to precede the table
 #' @param table_iss the issue table
 #' @param table_pr The PR table
 #' @param footer_ the text to follow the table
-#' @return the email body
+#' @seealso
+#' * [blastula::creds()]
+#' @return
+#' A string with the email body.
 #' @export
-build_email <- function(from_, to_, creds, body_, table_iss, table_pr, footer_) {
+build_email <- function(
+    from_,
+    to_,
+    creds,
+    body_,
+    table_iss,
+    table_pr,
+    footer_) {
   email <- blastula::compose_email(
     body = blastula::blocks(
       blastula::block_text(body),
